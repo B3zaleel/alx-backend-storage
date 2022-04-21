12aa06ed03c81bba55ef0d6845a9a681f431dc57
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-21 08:51:57 +0100

    :wrench: Change procedure's routine

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,10 +4,10 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_weighted_score FLOAT DEFAULT 0;
+    DECLARE total_weighted_score INT DEFAULT 0;
     DECLARE total_weight INT DEFAULT 0;
 
-    SELECT CAST(SUM(corrections.score * projects.weight) AS FLOAT)
+    SELECT SUM(corrections.score * projects.weight)
         INTO total_weighted_score
         FROM corrections
             INNER JOIN projects
@@ -20,8 +20,15 @@ BEGIN
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
-    UPDATE users
-        SET users.average_score = total_weighted_score / total_weight
-        WHERE users.id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
 END $$
 DELIMITER ;
