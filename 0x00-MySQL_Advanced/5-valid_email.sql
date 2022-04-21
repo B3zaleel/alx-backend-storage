646796ecd2ea9847fb2d453169b7b4aea74e1601
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-21 16:36:38 +0100

    :wrench: Change trigger's update routine

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,16 @@
 -- Creates a trigger that resets the attribute valid_email
 -- only when the email has been changed.
+-- @author Bezaleel Olakunori <https://github.com/B3zaleel>
 DROP TRIGGER IF EXISTS validate_email;
 DELIMITER $$
 CREATE TRIGGER validate_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    SET NEW.valid_email = IF(IFNULL(OLD.email, "") != IFNULL(NEW.email, ""), NOT NEW.valid_email, OLD.valid_email);
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
 END $$
 DELIMITER ;
