834fb0f8311cb94835426f970d815f14db005425
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-27 18:25:24 +0100

    :wrench: Refactor script

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,14 +16,9 @@ def get_page(url: str) -> str:
     '''
     req_key = 'count:{}'.format(url)
     res_key = 'result:{}'.format(url)
-    if redis_store.exists(req_key) == 0:
-        redis_store.set(req_key, 1)
-    else:
-        redis_store.incr(req_key, 1)
-    if redis_store.exists(res_key) != 0:
-        result = redis_store.get(res_key)
-        return result.decode('utf-8') if result else result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.set(res_key, result)
-    redis_store.expire(res_key, 10)
+    redis_store.incr(req_key)
+    result = redis_store.get(res_key)
+    if result is None:
+        result = requests.get(url).content.decode('utf-8')
+        redis_store.setex(res_key, 10, result)
     return result
