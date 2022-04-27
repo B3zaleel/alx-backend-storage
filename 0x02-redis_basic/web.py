4425b0cca1598c2598a2ee1425e1872bcdc7d773
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-27 19:01:01 +0100

    :wrench: Change when request count is incremented

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,12 +13,12 @@ def get_page(url: str) -> str:
     if url is None or len(url.strip()) == 0:
         return ''
     redis_store = redis.Redis()
-    req_key = 'count:{}'.format(url)
     res_key = 'result:{}'.format(url)
-    redis_store.incr(req_key)
     result = redis_store.get(res_key)
-    if result is None:
-        result = requests.get(url).content.decode('utf-8')
-        redis_store.setex(res_key, timedelta(seconds=10), result)
+    if result is not None:
         return result
-    return result.decode('utf-8')
+    req_key = 'count:{}'.format(url)
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.incr(req_key)
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
