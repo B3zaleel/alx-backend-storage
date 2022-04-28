9ba5e5d21d2296d8620d283fec2b7ccae5e7c1dd
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-28 03:56:17 +0100

    :wrench: Refactor script

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -19,9 +19,9 @@ def data_cacher(method: Callable) -> Callable:
     def invoker(url) -> str:
         '''The wrapper function for caching the output.
         '''
-        redis_store.incr(f'count:{url}')
         result = redis_store.get(f'result:{url}')
         if result:
+            redis_store.incr(f'count:{url}')
             return result.decode('utf-8')
         result = method(url)
         redis_store.setex(f'result:{url}', 10, result)
