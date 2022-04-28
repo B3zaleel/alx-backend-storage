bc0972e56c476cee70e9eba3517f1ac1b3becb44
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-28 03:55:26 +0100

    Revert "Testing checker"
    This reverts commit 4a2a47b3b14cad6d90e2c78ab8b1c6024bed8c97.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python3
 '''A module with tools for request caching and tracking.
 '''
-from functools import wraps
 import redis
 import requests
-from typing import Callable
+from functools import wraps
+from typing import Any, Callable
 
-redis_ = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decortator for counting """
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(url):
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-    return wrapper
 
-
-@count_requests
+@data_cacher
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    req = requests.get(url)
-    return req.text
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
